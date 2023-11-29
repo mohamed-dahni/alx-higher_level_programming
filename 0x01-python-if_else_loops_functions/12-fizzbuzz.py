27bafeedf31e7875ae9165d5a4e867367e0521b1
Mohamed <mdahni06@gmail.com>
2023-11-29 08:06:48 +0100

    +

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,13 +2,13 @@
 
 def fizzbuzz():
     str_to_print = ""
-    
+
     for i in range(0, 101):
         if (i % 3) == 0 and (i % 5) == 0:
             str_to_print += "FizzBuzz "
         elif (i % 3) == 0:
             str_to_print += "Fizz "
-        elif (i % 5):
+        elif (i % 5) == 0:
             str_to_print += "Buzz "
         else:
             str_to_print += f"{i} "
