84951e0191dec15aa3e8c2b151d7244b0b16de83
Mohamed <mdahni06@gmail.com>
2023-11-29 04:26:02 +0100

    +

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,15 +5,12 @@ def get_reversed_num(number):
 
 
 def main():
-    unique_list = []
+    for i in range(0, 10):
+        for j in range(i, 10):
+            if i != j:
+                print("{}{}, ".format(i, j), end="")
 
-    for i in range(0, 100):
-        num = "{:02d}".format(i)
-        if num[0] != num[1] and num not in unique_list and get_reversed_num(num) not in unique_list:
-            unique_list.append(num)
-
-    print("{}".format(", ".join(unique_list)))
-
+    print()
 
 if __name__ == "__main__":
     main()
