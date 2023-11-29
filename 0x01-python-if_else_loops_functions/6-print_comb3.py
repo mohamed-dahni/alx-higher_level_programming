8e13903351d9e96eabe9cebaadace5037a74a959
Mohamed <mdahni06@gmail.com>
2023-11-29 04:31:07 +0100

    +

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
-def get_reversed_num(number):
-    return number[::-1]
-
-
 def main():
     for i in range(0, 10):
         for j in range(i, 10):
             if i != j:
-                print("{}{}, ".format(i, j), end="")
+                print("{}{}".format(i, j), end="")
+                if i != 8 and j != 9:
+                    print("{}".format(", "), end="")
 
     print()
+
 
 if __name__ == "__main__":
     main()
