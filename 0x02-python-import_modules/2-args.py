fab896ee1efb695fd28f60c0bffa31d536e4ea68
Mohamed <mdahni06@gmail.com>
2023-12-01 01:29:13 +0100

    +

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,10 @@ from sys import argv
 
 def main():
     arg_count = len(argv) - 1
-    print("{} argument.".format(arg_count))
+    if arg_count == 1:        
+        print("{} argument.".format(arg_count))
+    else:
+        print("{} arguments.".format(arg_count))
           
     for i, arg in enumerate(argv[1:], start=1):
         print("{}: {}".format(i, arg))
