f3bd5b14c67ee85dc8b0a069ae42507db32e24b3
Mohamed <mdahni06@gmail.com>
2023-12-01 01:11:05 +0100

    +

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+
+def main():
+    print("{} arguments.".format(len(argv))
+          
+    for i, arg in argv:
+        print("{}: {}".format(i + 1, arg))
+
+
+if __name__ == "__main__":
+    main()
