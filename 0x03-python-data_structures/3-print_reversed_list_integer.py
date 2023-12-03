7ba9ad95b2c6fa83cd7898bd815424ca60180315
Mohamed <mdahni06@gmail.com>
2023-12-03 23:09:56 +0100

    +

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,7 @@
 def new_in_list(my_list, idx, element):
     if not my_list or idx > len(my_list)-1 or idx < 0:
         return my_list
-    
+
     new_list = my_list.copy()
     new_list[idx] = element
 
