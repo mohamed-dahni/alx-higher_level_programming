2ebcbcd9406a5a46a27c4b3cb28aae351741019d
Mohamed <mdahni06@gmail.com>
2023-12-07 01:10:58 +0100

    +

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+import sys
+import hidden_4
 
-if __name__ == "__main__":
-    for i in dir():
-        if not i.startswith("__"):
-            print(i)
+if __name__ != "__main__":
+    exit()
+
+for i in dir(hidden_4):
+    if not i.startswith("__"):
+        print(i)
