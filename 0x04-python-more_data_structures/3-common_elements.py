6f1200dbd00ba32c4133ffa703a6c71e6db84d23
Mohamed <mdahni06@gmail.com>
2023-12-07 02:10:35 +0100

    +

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    common_elements = set()
+
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                common_elements.add(i)
+
+    return common_elements
