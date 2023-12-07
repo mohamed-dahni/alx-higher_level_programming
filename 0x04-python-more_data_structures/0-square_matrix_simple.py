2284715a0b84b116a6a6779fd715d59b83bb7adb
Mohamed <mdahni06@gmail.com>
2023-12-07 01:48:12 +0100

    +

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for i in matrix:
+        row = []
+        for j in i:
+            row.append(j**2)
+        new_matrix.append(row)
+    
+    return new_matrix
