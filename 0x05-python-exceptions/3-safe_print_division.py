c31fc798001e405d7ff21f1a02a33d273c4c501f
Mohamed <mdahni06@gmail.com>
2023-12-19 02:29:33 +0100

    +

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        return result
+def safe_print_division(a, b):
+    result = None
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        return result
