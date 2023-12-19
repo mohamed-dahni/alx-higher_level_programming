a3d1a9d4d3063593b389e625f8b429cc69028160
Mohamed <mdahni06@gmail.com>
2023-12-19 23:13:48 +0100

    +

--- 0x06-python-classes/4-square copy.py
+++ 0x06-python-classes/4-square copy.py
@@ -14,6 +14,17 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = size
 
+    def my_print(self):
+        """
+        Prints the square with the # character by iterating over
+        the size of the square.
+        """
+        if self.__size == 0:
+            return
+        for i in range(self.__size):
+            print("#" * self.__size)
+
+
     def area(self):
         """Returns the area of the square."""
         return self.__size ** 2
