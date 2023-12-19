edcc120377c1a7cf8675204b699e8ec801086ec5
Mohamed <mdahni06@gmail.com>
2023-12-19 01:40:57 +0100

    +

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     printed_count = 0
+
     try:
         for i in range(x):
             print("{}".format(my_list[i]))
@@ -7,5 +10,5 @@ def safe_print_list(my_list=[], x=0):
         print()
     except:
         print("error")
-    
+
     return printed_count
