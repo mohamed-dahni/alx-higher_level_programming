ce08b22962bdfaaf86f450a8b300164b286e8951
Mohamed <mdahni06@gmail.com>
2023-12-19 22:01:37 +0100

    +

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    __init__(self, size):
+        """Initializes the square."""
+        self.__size = size
