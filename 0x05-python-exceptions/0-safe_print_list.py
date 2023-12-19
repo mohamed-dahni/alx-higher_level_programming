adc972039aaca19a581a82c96ac3ac1957f100f1
Mohamed <mdahni06@gmail.com>
2023-12-19 01:40:05 +0100

    +

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    printed_count = 0
+    try:
+        for i in range(x):
+            print("{}".format(my_list[i]))
+            printed_count = printed_count + 1
+        print()
+    except:
+        print("error")
+    
+    return printed_count
