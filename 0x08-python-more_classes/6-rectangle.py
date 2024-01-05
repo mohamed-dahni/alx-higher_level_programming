01dcc7155727179b11934a078cff4dc9d3c77523
Mohamed <mdahni06@gmail.com>
2024-01-05 03:51:58 +0100

    +

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
         """ init method """
         self.height = height
         self.width = width
-        self.__class__.number_of_instances -= + 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -63,5 +63,5 @@ class Rectangle:
 
     def __del__(self):
         """ prints a message when the object is destroyed """
-        self.__class__.number_of_instances -= 1
+        type(self).number_of_instances -= 1
         print("Bye rectangle...")
