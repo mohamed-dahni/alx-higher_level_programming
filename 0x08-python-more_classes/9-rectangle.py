65738fb8a734e20397b9151fb3b770d2d1ee9072
Mohamed <mdahni06@gmail.com>
2024-01-05 18:44:49 +0100

    +

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -59,6 +59,7 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         return rect_1 if rect_1.area() >= rect_2.area() else rect_2
 
+    @classmethod
     def square(cls, size=0):
         """ returns a square """
         return cls(size, size)
