e954e290cf9d43ff3a4fd2764dabfdab3b6a0af1
Mohamed <mdahni06@gmail.com>
2024-01-05 03:39:45 +0100

    +

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -54,3 +54,7 @@ class Rectangle:
             return ""
         return ("\n".join(["".join(["#" for i in range(self.__width)])
                 for j in range(self.__height)]))
+
+    def __repr__(self):
+        """ returns string representation of Rectangle """
+        return "Rectangle({}, {})".format(self.__width, self.__height)
