122157aa35f49971144405dd2b56a7c773f39782
Mohamed <mdahni06@gmail.com>
2024-01-05 03:37:46 +0100

    +

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,10 +50,7 @@ class Rectangle:
 
     def __str__(self):
         """ returns string representation of Rectangle """
-        if self.__width == 0 or self.__height == 0:
+        if self.__width is 0 or self.__height is 0:
             return ""
-
-        rec = ""
-        for i in range(self.__height):
-            rec = rec + "#" * self.__width + "\n"
-        return rec
+        return ("\n".join(["".join(["#" for i in range(self.__width)])
+                for j in range(self.__height)]))
