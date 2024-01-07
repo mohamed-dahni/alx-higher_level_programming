9b1c27c5d7bc9d70e0b56c908a7fa9afaeccc48a
Mohamed <mdahni06@gmail.com>
2024-01-07 22:07:43 +0100

    +

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Define a function that adds two integers and returns the result"""
+
+def add_integer(a, b=98):
+    """Add two integers and return the result"""
+    return a + b
